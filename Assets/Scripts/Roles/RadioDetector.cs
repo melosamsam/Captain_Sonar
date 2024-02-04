660266a902df2f032ae05993d81f444307527d84
bluesamsam <103419843+melosamsam@users.noreply.github.com>
2024-02-04 18:29:36 +0100

    Corrected Grid appearance

--- Assets/Scripts/Roles/RadioDetector.cs
+++ Assets/Scripts/Roles/RadioDetector.cs
@@ -80,6 +80,7 @@ public class RadioDetector : Role
         _isSeeThroughOpen = false;
         _isGridOpen = false;
         _isDotOpen = false;
+        _Grid.transform.localScale = Vector3.zero;
     }
 
     // Update is called once per frame
